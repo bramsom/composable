82e927503892d16ae35d62c708554fafdde4e214
bramsom <brayansmartinez0607@gmail.com>
2024-07-25 12:08:46 -0500

    envio del proyecto android

---
+++ app/src/main/java/com/adso/a1891_composable/MainActivity.kt
@@ -0,0 +1,123 @@
+package com.adso.a1891_composable
+
+import android.os.Bundle
+import android.view.Surface
+import android.widget.Button
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.animation.expandIn
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.material3.Button
+import androidx.compose.material3.ElevatedButton
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.material3.surfaceColorAtElevation
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.adso.a1891_composable.ui.theme._1891_composableTheme
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            _1891_composableTheme {
+                MyApp(modifier = Modifier.fillMaxSize())
+                }
+            }
+        }
+    }
+
+@Composable
+fun MyApp(modifier: Modifier=Modifier){
+
+        Greetings()
+}
+
+@Composable
+private fun Greetings(
+    modifier: Modifier = Modifier,
+    names: List<String> = listOf("World", "Compose")
+) {
+    Column(modifier = modifier.padding(vertical = 4.dp)) {
+        for (name in names) {
+            Greeting(name = name)
+        }
+    }
+}
+
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    val expanded = remember {  mutableStateOf(false) }
+    val extraPadding = if (expanded.value) 48.dp else 0.dp//dont do this
+    Surface(
+        color = MaterialTheme.colorScheme.primary,
+        modifier = modifier.padding(vertical = 4.dp, horizontal = 8.dp)
+    ) {
+        Row(modifier = Modifier.padding(24.dp)) {
+            Column(modifier = modifier.weight(1f).padding(bottom = extraPadding)) {
+                Text(text = "hello")
+                Text(text = name)
+            }
+            ElevatedButton(
+                onClick = {expanded.value =!expanded.value}
+            ){
+                Text(if(expanded.value)"Show less" else "Show more" )
+            }
+        }
+    }
+}
+
+@Composable
+fun OnboardingScreen(modifier: Modifier = Modifier) {
+    // TODO: This state should be hoisted
+    var shouldShowOnboarding by remember { mutableStateOf(true) }
+
+    Column(
+        modifier = modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Text("Welcome to the Basics Codelab!")
+        Button(
+            modifier = Modifier.padding(vertical = 24.dp),
+            onClick = { shouldShowOnboarding = false }
+        ) {
+            Text("Continue")
+        }
+    }
+}
+@Preview(showBackground = true, widthDp = 320, heightDp = 320 )
+@Composable
+fun GreetingPreview() {
+    _1891_composableTheme {
+        Greetings()
+    }
+}
+
+@Preview
+@Composable
+fun MyAppPreview() {
+    _1891_composableTheme {
+        MyApp(Modifier.fillMaxSize())
+    }
+}
+
